6f2835f86df630eb896a2b5de2dd5066bd1d246c
aprogergely <81948551+aprogergely@users.noreply.github.com>
2023-03-18 17:00:08 +0100

    divided main screen to 2 frames
    moved all widgets onto a frame located at the right side of the screen
    made a currently empty "frame" on the left side of the screen, we'll store everything other than the chat on this side in the future

--- chat app/chat.py
+++ chat app/chat.py
@@ -12,21 +12,36 @@ class MyApp:
             with open("chathistory.json", "r") as f:
                 chathistory = json.load(f)
         except:
-            with open("chathistory.json", "x") as f:
-                chathistory = json.load(f)
+            with open("chathistory.json", "w") as f:
+                chathistory = {}
+                json.dump(chathistory, f)
 
         self.username = username
 
         # Create a new Tkinter window
         self.window = tk.Tk()
         self.window.title("Chat")
+        self.window.columnconfigure([0,1], minsize=30, weight=1)
+        self.window.columnconfigure(2, minsize=300, weight=1)
+        self.window.rowconfigure([0,1,2], minsize=30, weight=1)
+
+        # frames
+        frameL = tk.Frame(self.window, bg="red")
+        frameR = tk.Frame(self.window, bg="yellow")
+        frameL.grid(row=1,column=1)
+        frameR.grid(row=1,column=2)
+        frameL.columnconfigure([0,1,2,3], minsize=30, weight=1)
+        frameR.columnconfigure([0,1,2], minsize=30, weight=1)
+        frameR.rowconfigure(1, minsize=300, weight=1)
+        frameL.rowconfigure([0,1,2,3], minsize=30, weight=1)
+        frameR.rowconfigure([0,2,3], minsize=30, weight=1)
 
         # Create a text box for displaying chat history and another for inputtin new messages
-        text_box_chat = tk.Text(self.window)
-        text_box_entry = tk.Entry(self.window)
+        text_box_chat = tk.Text(master=frameR)
+        text_box_entry = tk.Entry(master=frameR)
 
-        text_box_chat.place(x=50, y=50, width=200, height=300)
-        text_box_entry.place(x=50, y=400, width=200, height=30)
+        text_box_chat.grid(row=1,column=2,sticky=W)
+        text_box_entry.grid(row=2,column=2)
 
         for i in chathistory:
             text_box_chat.insert(tk.END, chathistory[i]["Name"]+"\n")
@@ -35,8 +50,8 @@ class MyApp:
             text_box_chat.insert(tk.END, "\n")
 
         # Create a scrollbar for viewing ealier messages not vissible in the textbox
-        scrollbar = ttk.Scrollbar(self.window, orient=VERTICAL, command=text_box_chat.yview)
-        scrollbar.place(x=300, y=50, width=20, height=300)
+        scrollbar = ttk.Scrollbar(master=frameR, orient=VERTICAL, command=text_box_chat.yview)
+        scrollbar.grid(row=1,column=2,sticky=N+S+E)
 
         text_box_chat['yscrollcommand']=scrollbar.set
 
@@ -46,8 +61,10 @@ class MyApp:
             newtext = text_box_entry.get()
             x = datetime.datetime.now()
             # Print the text to the console
-            chathistory[x.strftime("%f")]["Name"]=username
-            chathistory[x.strftime("%f")]["Text"]=newtext
+            microseconds=x.strftime("%f")
+            chathistory[microseconds]={}
+            chathistory[microseconds]["Name"]=username
+            chathistory[microseconds]["Text"]=newtext
             with open("chathistory.json", "w") as f:
                 json.dump(chathistory,f)
             text_box_chat.insert(tk.END, username + "\n")
@@ -57,8 +74,8 @@ class MyApp:
             text_box_entry.delete(0, tk.END)
 
 
-        try_login_button = tk.Button(self.window, text="Submit", command=on_button_click)
-        try_login_button.place(x=250, y=400, width=50, height=30)
+        post_comment_button = tk.Button(master=frameR, text="Submit", command=on_button_click)
+        post_comment_button.grid(row=4,column=2)
 
         # Start the main event loop of the window
     def start(self):
--- chat app/start.py
+++ chat app/start.py
@@ -3,20 +3,20 @@ import registering
 import tkinter as tk
 
 
-window = tk.Tk()
-window.title("Loading...")
+#window = tk.Tk()
+#window.title("Loading...")
 
-def load(event):
-    nextwindow=""
-    window.destroy()
-    try:
+#def load(event):
+    #nextwindow=""
+    #window.destroy()
+try:
         savedaccount = open("account.txt", "r")
         nextwindow = chat.MyApp(savedaccount.readline())
         nextwindow.start()
-    except:
+except:
         nextwindow = registering.MyApp()
         nextwindow.start()
 
-window.bind("<Key>", load)
+#window.bind("<Key>", load)
 
-window.mainloop()
+#window.mainloop()
